c2ad63df2096987e4bc8c21d790608eda53338cb
Rafael <rafael.blomer@gmail.com>
2024-10-16 09:10:40 -0300

    commit inicial

---
+++ src/main/java/com/futebas/servicousuario/ServicoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.futebas.servicousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futebas/servicousuario/ServicoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futebas.servicousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
